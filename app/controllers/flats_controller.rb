008cf48bad268db0e23943f415300984e1e25146
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-07 19:47:13 -0300

    Controller's functions created - index, show and set_flats

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,19 @@
+require 'open-uri'
+
 class FlatsController < ApplicationController
+  MY_FLATS_URL = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json".freeze
+  before_action :set_flats
+
+  def index
+  end
+
+  def show
+    @flat = @flats.find { |flat| flat["id"] == params[:id].to_i }
+  end
+
+  private
+
+  def set_flats
+    @flats = JSON.parse(open(MY_FLATS_URL).read)
+  end
 end
